71e601731eea94edd21eada7bc307ef8c2ca8000
丁川坤 <2310107723@qq.com>
2020-02-03 16:38:52 +0800

    add index and  bootstrap

--- src/main/java/com/dck/community/controller/HelloController.java
+++ src/main/java/com/dck/community/controller/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping
-    public String hello(@RequestParam(name ="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+//  显示主页
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
