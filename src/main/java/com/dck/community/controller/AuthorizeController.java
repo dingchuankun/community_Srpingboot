963d96e0c0f604988df96f9a2c931a49a47dd768
丁川坤 <2310107723@qq.com>
2020-02-05 21:48:39 +0800

    publish

--- src/main/java/com/dck/community/controller/AuthorizeController.java
+++ src/main/java/com/dck/community/controller/AuthorizeController.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -30,7 +33,8 @@ public class AuthorizeController {
     @GetMapping("callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
@@ -42,14 +46,16 @@ public class AuthorizeController {
         if(githubUser != null){
             //登陆成功
             User user=new User();
-            user.setToken(UUID.randomUUID().toString());
+            final String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             System.out.println("输出user："+user);
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            //写入cookie
+            response.addCookie(new Cookie("token",token));
             System.out.println(githubUser);
             return  "redirect:/";
         }else {
--- src/main/java/com/dck/community/controller/HelloController.java
+++ src/main/java/com/dck/community/controller/HelloController.java
@@ -1,15 +1,39 @@
 package com.dck.community.controller;
 
+import com.dck.community.mapper.UserMapper;
+import com.dck.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
+    @Autowired
+    private UserMapper userMapper;
+
 //  显示主页
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user =userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                };
+                break;
+            };
+
+        }
+
         return "index";
+
     }
 }
